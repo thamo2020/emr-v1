7aadcb6ffbbdd49a7d6a26c3646d557b6d115361
thamo2020 <thamodya01@gmail.com>
2021-11-28 23:36:47 +0530

    updating patient history records

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+<h2>Patient History</h2>
+
 <table class="table table-striped">
     <thead>
       <tr>
@@ -21,8 +23,44 @@
       </tr>
     </thead>
     <tbody>
+
+      <!--tr>
+        <th scope="row">3</th>
+        <th>Fever</th>
+        <td>Viral fever</td>
+        <td>110/70</td>
+        <td>96/mm</td>
+        <td>18/mm</td>
+        <td>Eaminarion - Normal</td>
+        <td>FBC <br>CRP <br> UFR </td>
+        <td>Paracetamol 2tab 6hours 3d <br>
+          Piriton 4mg 12hours 3d
+          </td>
+        <td>No</td>
+        <td>2021-11-29 </td>
+        <td>2021-11-29 </td>
+      </!--tr-->
+
+      <tr>
+        <th scope="row">2</th>
+        <th>Abdominal pain, Fever</th>
+        <td>Urinary tracr injection</td>
+        <td>150/90</td>
+        <td>84/mm</td>
+        <td>18</td>
+        <td>Lower abdominal tenderness</td>
+        <td>FBC <br> UFR</td>
+        <td>Paracetamol 2tab 6hours 3d <br>
+        Crprofioxacin 500mg 12hours 3d
+        </td>
+        <td>No</td>
+        <td>2021-11-01 </td>
+        <td>2021-11-01 </td>
+      </tr>
+
       <tr>
         <th scope="row">1</th>
+        <th>Headache</th>
         <td>Migraine</td>
         <td>120/76</td>
         <td>72/mm</td>
@@ -34,36 +72,12 @@
         Famotidine 20mg 12hours 3d <br>
         </td>
         <td>No</td>
-        <td>2021-10-31 07:24:50</td>
-        <td>2021-10-31 07:24:5</td>
+        <td>2021-10-25 </td>
+        <td>2021-10-25 </td>
 
       </tr>
-      <tr>
-        <th scope="row">1</th>
-        <td>John</td>
-        <td>65</td>
-        <td>102</td>
-        <td>102</td>
-        <td>No</td>
-        <td>No</td>
-        <td>No</td>
-        <td>No</td>
-        <td>2021-10-31 07:24:50</td>
-        <td>2021-10-31 07:24:5</td>
-      </tr>
-      <tr>
-        <th scope="row">1</th>
-        <td>Smith</td>
-        <td>65</td>
-        <td>102</td>
-        <td>102</td>
-        <td>No</td>
-        <td>No</td>
-        <td>No</td>
-        <td>No</td>
-        <td>2021-10-31 07:24:50</td>
-        <td>2021-10-31 07:24:5</td>
-      </tr>
+      
+     
     </tbody>
   </table>
 
